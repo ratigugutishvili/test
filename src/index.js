4d4864ba2671139450fe6d33ce2abdf4fd6f1a42
ratigugutishvili <ratigugutishvili2004@mail.ru>
2023-11-27 08:00:45 +0400

    updated

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     element: <App prop={{color:'main'}} />
   },
   {
-    path: "/production",
+    path: "https://urchin-app-rg8ub.ondigitalocean.app/production",
     element: <Production prop={{color:'rediko'}} />
   },
 ])
