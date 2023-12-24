bdcb3a0eb63419ab51ed60c50eefa8c7d651d880
ratigugutishvili <ratigugutishvili2004@mail.ru>
2023-12-24 05:11:36 +0400

    ll

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,19 +1,45 @@
 import Header from "./header";
-import '../contact.css';
-import phone from "../phone.png"
+import "../contact.css";
+import phone from "../phone.png";
+import loc from "../location-pin.png";
+import env from "../envelope.png";
 
-export default function Contact(){
-    return(
-        <div>
-            <Header prop={{ color: "contact" }} />
-            <div className="contact-span">
-                <span>დ ა გ ვ ი კ ა ვ შ ი რ დ ი თ:</span>
-            </div>
-            <div>
-                <div className="contact-flex"><img src={phone} alt="" srcset="" /><span>34523465436 </span></div>
-                <div className="contact-flex"></div>
-                <div className="contact-flex"></div>
-            </div>
+export default function Contact() {
+  return (
+    <div>
+      <Header prop={{ color: "contact" }} />
+      <div className="contact-span">
+        <span>დ ა გ ვ ი კ ა ვ შ ი რ დ ი თ:</span>
+      </div>
+      <div className="contact-body">
+        <div className="d-flex-conc">
+          <div className="contact-flex">
+            <img src={phone} alt="" srcset="" />
+            <span>34523465436 </span>
+          </div>
+          <div className="contact-flex contact-margin">
+            <img src={phone} alt="" srcset="" />
+            <span>34523465436 </span>
+          </div>
+          <div className="contact-flex">
+            <img src={phone} alt="" srcset="" />
+            <span>34523465436 </span>
+          </div>
         </div>
-    )
+        <div className="contact-flexa contact-2">
+          <span>
+            <img src={loc} alt="" srcset="" />
+            იყალთოს პირველი შესახვევი #5 <br /> საბურთალო, Tbilisi, Georgia
+          </span>
+        </div>
+        <div className="contact-flexa contact-3">
+          <img src={env} alt="" srcset="" />
+          <span>იყალთოს პირველი შესახვევ</span>
+        </div>
+      </div>
+      <div className="contact-button">
+        <button className="conc-btn">შეუკვეთე ქავთასი ახლავე</button>
+      </div>
+    </div>
+  );
 }
