5821ea05f239e3752217796520e64525df1b78ed
ratigugutishvili <ratigugutishvili2004@mail.ru>
2024-02-19 04:25:45 +0400

    sdf

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,89 +7,87 @@ import "../footer.css";
 export default function Footer() {
   const navigate = useNavigate();
   return (
-    <div className="mobile-footer">
-      <div className="footer">
-        <div className="footer-1">
-          <span
-            onClick={() => {
-              navigate("/production");
-            }}
-          >
-            პროდუქცია
-          </span>
-          <span
-            onClick={() => {
-              navigate("/blogs");
-            }}
-          >
-            ბლოგი
-          </span>
-          <span
-            onClick={() => {
-              navigate("/aboutus");
-            }}
-          >
-            ჩვენ შესახებ
-          </span>
+    <div>
+      <div className="mobile-footer">
+        <div className="footer">
+          <div className="footer-1">
+            <span
+              onClick={() => {
+                navigate("/production");
+              }}
+            >
+              პროდუქცია
+            </span>
+            <span
+              onClick={() => {
+                navigate("/blogs");
+              }}
+            >
+              ბლოგი
+            </span>
+            <span
+              onClick={() => {
+                navigate("/aboutus");
+              }}
+            >
+              ჩვენ შესახებ
+            </span>
+          </div>
+          <div className="footer-2">
+            <span className="footer-2span">კონტაქტი :</span>
+            <div className="footer-flex">
+              <div>
+                <img src={phone} alt="" srcset="" />
+                551 611 416
+              </div>
+              <div>
+                <img src={face} alt="" srcset="" />
+                ქავთასი • Kavtasi
+              </div>
+              <div className="footer-center">
+                <img src={loc} alt="" srcset="" />
+                თბილისი, ალექსანდრე ყაზბეგის გამზირი - 32ა
+              </div>
+            </div>
+          </div>
+          <div className="footer-3">
+            <button
+              onClick={() => {
+                navigate("/buy");
+              }}
+            >
+              შეუკვეთე ახლავე
+            </button>
+          </div>
         </div>
-        <div className="footer-2">
-          <span className="footer-2span">კონტაქტი :</span>
-          <div className="footer-flex">
+        <div className="end-footer">კლინიკა ქავთასი გისურვებთ ჯანმრთელობას</div>
+      </div>
+      <div className="for-mobile-footer">
+        <div className="mobile-main-footer">
+          <div className="mobile-footer-three">
+            <span>პროდუქცია</span>
+            <span>ბლოგი</span>
+            <span>ჩვენ შესახებ</span>
+          </div>
+          <div className="mobile-footer-icons">
+            <span className="mobile-position">კონტაქტი</span>
             <div>
               <img src={phone} alt="" srcset="" />
-              551 611 416
+              <span>555-95-85-75</span>
             </div>
             <div>
               <img src={face} alt="" srcset="" />
-              ქავთასი • Kavtasi
+              <span>ქავთასი • Kavtasi</span>
             </div>
-            <div className="footer-center">
+            <div>
               <img src={loc} alt="" srcset="" />
-              თბილისი, ალექსანდრე ყაზბეგის გამზირი - 32ა
+              <span className="txtcenter">
+                იყალთოს პირველი <br /> შესახვევი #5 საბურთალო,
+                <br />
+                Tbilisi, Georgia
+              </span>
             </div>
           </div>
-        </div>
-        <div className="footer-3">
-          <button
-            onClick={() => {
-              navigate("/buy");
-            }}
-          >
-            შეუკვეთე ახლავე
-          </button>
-        </div>
-      </div>
-      <div className="end-footer">კლინიკა ქავთასი გისურვებთ ჯანმრთელობას</div>
-      <div className="for-mobile-footer">
-        <div className="mobile-main-footer">
-            <div className="mobile-footer-three">
-                <span>
-                    პროდუქცია
-                </span>
-                <span>
-                    ბლოგი
-                </span>
-                <span>
-                    ჩვენ შესახებ
-                </span>
-            </div>
-            <div className="mobile-footer-icons">
-                <span className="mobile-position">
-                    კონტაქტი
-                </span>
-                <div>
-                    <img src={phone} alt="" srcset="" />
-                    <span>555-95-85-75</span>
-                </div>
-                <div>
-                    <img src={face} alt="" srcset="" />
-                    <span>ქავთასი • Kavtasi</span>
-                </div>
-                <div >
-                    <img src={loc} alt="" srcset="" />
-                    <span className="txtcenter">იყალთოს პირველი <br /> შესახვევი #5 საბურთალო,<br />Tbilisi, Georgia</span>
-                </div>
-            </div>
         </div>
       </div>
     </div>
